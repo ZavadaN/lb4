ae1a97793e5cfc1f6d93289ad4aaa00062c8fda8
ZavadaN <113342158+ZavadaN@users.noreply.github.com>
2022-10-27 04:17:12 +0300

    Add files via upload

---
+++ tests/test_LB4.py
@@ -0,0 +1,109 @@
+import re
+import inspect
+import LB3
+
+import pytest
+
+class Test_Calculator:
+
+    def test_init(self, init_limit, msg_err):
+        assert hasattr(LB3, 'Calculator'), msg_err('add_class', 'Calculator')
+        result = LB3.Calculator(init_limit)
+        assert hasattr(result, 'limit'), msg_err('add_attr', 'limit', 'Calculator')
+        assert result.limit == init_limit, msg_err('wrong_attr', 'limit', 'Calculator')
+        assert hasattr(result, 'records'), msg_err('add_attr', 'records', 'Calculator')
+        assert result.records == [], msg_err('wrong_attr', 'records', 'Calculator')
+
+        assert not hasattr(result, 'USD_RATE'), msg_err('dont_create_attr', 'USD_RATE', 'Calculator')
+        assert not hasattr(result, 'EURO_RATE'), msg_err('dont_create_attr', 'EURO_RATE', 'Calculator')
+
+    def test_add_record(self, init_limit, data_records, msg_err):
+        result = LB3.Calculator(init_limit)
+        assert hasattr(result, 'add_record'), msg_err('add_method', 'add_record', 'Calculator')
+        records, today, week = data_records
+        for record in records:
+            result.add_record(record)
+        assert result.records == records, msg_err('wrong_attr', 'records', 'Calculator')
+
+    def test_get_today_stats(self, init_limit, data_records, msg_err):
+        result = LB3.Calculator(init_limit)
+        records, today, week = data_records
+        for record in records:
+            result.add_record(record)
+        assert hasattr(result, 'get_today_stats'), msg_err('add_method', 'get_today_stats', 'Calculator')
+        assert result.get_today_stats() == today, msg_err('wrong_method', 'get_today_stats', 'Calculator')
+
+    def test_get_week_stats(self, init_limit, data_records, msg_err):
+        result = LB3.Calculator(init_limit)
+        records, today, week = data_records
+        for record in records:
+            result.add_record(record)
+        assert hasattr(result, 'get_week_stats'), msg_err('add_method', 'get_week_stats', 'Calculator')
+        assert result.get_week_stats() == week, msg_err('wrong_method', 'get_week_stats', 'Calculator')
+        get_week_stats_inspect = inspect.getsource(result.get_week_stats)
+        get_week_stats_inspect_in_class = inspect.getsource(LB3.Calculator)
+        assert (
+            'days=7' in get_week_stats_inspect or
+            'weeks=1' in get_week_stats_inspect or
+            'days=7' in get_week_stats_inspect_in_class or
+            'weeks=1' in get_week_stats_inspect_in_class
+        ), 'Нужно считать, сколько денег было потрачено за неделю'
+
+    def test_get_calories_remained(self, init_limit, msg_err):
+        result = LB3.Calculator(init_limit)
+        assert not hasattr(result, 'get_calories_remained'), (
+            msg_err('dont_create_method', 'get_calories_remained', 'Calculator')
+        )
+
+    def test_get_today_cash_remained(self, init_limit, msg_err):
+        result = LB3.Calculator(init_limit)
+        assert not hasattr(result, 'get_today_cash_remained'), (
+            msg_err('dont_create_method', 'get_today_cash_remained', 'Calculator')
+        )
+
+
+
+class Test_CashCalculator:
+
+    def test_init(self, init_limit, msg_err):
+        assert hasattr(LB3, 'CashCalculator'), (
+            msg_err('add_class', 'CashCalculator', child=True, parent_name='Calculator')
+        )
+        result = LB3.CashCalculator(init_limit)
+        assert hasattr(result, 'limit'), msg_err('child_method', 'CashCalculator', 'Calculator')
+        assert result.limit == init_limit, msg_err('child_method', 'CashCalculator', 'Calculator')
+
+        assert hasattr(result, 'EURO_RATE'), msg_err('add_attr', 'EURO_RATE', 'CashCalculator')
+        assert type(result.EURO_RATE) == float, msg_err('wrong_attr', 'EURO_RATE', 'CashCalculator')
+        assert result.EURO_RATE > 0, msg_err('wrong_attr', 'EURO_RATE', 'CashCalculator',
+                                             msg=', курс не может быть равен или меньше 0')
+
+        assert hasattr(result, 'USD_RATE'), msg_err('add_attr', 'USD_RATE', 'CashCalculator')
+        assert type(result.USD_RATE) == float, msg_err('wrong_attr', 'USD_RATE', 'CashCalculator')
+        assert result.USD_RATE > 0, msg_err('wrong_attr', 'USD_RATE', 'CashCalculator',
+                                            msg=', курс не может быть равен или меньше 0')
+
+    @pytest.mark.parametrize("amount,currency", [
+        (0, 'usd'), (0, 'eur'), (0, 'rub'),
+        (1, 'usd'), (1, 'eur'), (1, 'rub'),
+        (-1, 'usd'), (-1, 'eur'), (-1, 'rub')
+    ])
+    def test_get_today_cash_remained(self, init_limit, data_records, amount, currency, today_cash_remained, msg_err,
+                                     monkeypatch):
+        result = LB3.CashCalculator(init_limit)
+        assert hasattr(result, 'get_today_cash_remained'), (
+            msg_err('add_method', 'get_today_cash_remained', 'CashCalculator')
+        )
+
+        records, today, week = data_records
+        for record in records:
+            result.add_record(record)
+
+        result.EURO_RATE = 56
+        monkeypatch.setattr(LB3.CashCalculator, "EURO_RATE", 56)
+        result.USD_RATE = 58
+        monkeypatch.setattr(LB3.CashCalculator, "USD_RATE", 58)
+        result.limit = today + (amount * 300)
+        assert re.fullmatch(today_cash_remained(amount, currency), result.get_today_cash_remained(currency)), (
+            msg_err('wrong_method', 'get_today_cash_remained', 'CashCalculator')
+        )
